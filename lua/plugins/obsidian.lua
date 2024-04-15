8ef7e3270a253b633c058d7e7be880403b0b3409
Stefan Maaßen <s.maassen@verband.creditreform.de>
2024-04-15 08:44:48 +0200

    added better obsididan integration

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -8,17 +8,14 @@ return {
       -- Required.
       "nvim-lua/plenary.nvim",
     },
+    cmd = { "ObsidianOpen", "ObsidianNew", "ObsidianSearch", "ObsidianQuickSwitch", "ObsidianToday" },
     keys = {
-      {
-        "gl",
-        function()
-          if require("obsidian").util.cursor_on_markdown_link() then
-            return "<cmd>ObsidianFollowLink<CR>"
-          else
-            return "gl"
-          end
-        end,
-      },
+      { "<leader>oO", "<cmd>ObsidianOpen<CR>", desc = "Open Obsidian" },
+      { "<leader>on", "<cmd>ObsidianNew<CR>", desc = "New note" },
+      { "<leader>os", "<cmd>ObsidianSearch<CR>", desc = "Search" },
+      { "<leader>oo", "<cmd>ObsidianQuickSwitch<CR>", desc = "Find note" },
+      { "<leader>ot", "<cmd>ObsidianToday<CR>", desc = "Open Today" },
+      { "<leader>oT", "<cmd>ObsidianTemplate<CR>", desc = "Insert from template" },
     },
     opts = function()
       local options = {
@@ -55,6 +52,43 @@ return {
           return out
         end,
         use_advanced_uri = false,
+        mappings = {
+          ["gf"] = {
+            action = function()
+              if require("obsidian").util.cursor_on_markdown_link() then
+                return "<cmd>ObsidianFollowLink<CR>"
+              else
+                return "gf"
+              end
+            end,
+            opts = { noremap = false, expr = true, buffer = true },
+          },
+          -- Toggle check-boxes.
+          ["<CR>"] = {
+            action = function()
+              local line = vim.api.nvim_get_current_line()
+              for _, char in ipairs({ " ", "x", "~", ">", "-" }) do
+                if string.match(line, "^%s*- %[" .. char .. "%].*") then
+                  return "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>"
+                end
+              end
+              return "<CR>"
+            end,
+            opts = { noremap = false, expr = true, buffer = true },
+          },
+        },
+        ui = {
+          checkboxes = {
+            -- NOTE: the 'char' value has to be a single character
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+
+            -- You can also add more custom ones...
+          },
+          external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        },
       }
       if "stefan" == vim.env.USER then
         options.dir = vim.env.HOME .. "/Dokumente/obsidian/my-vault"
