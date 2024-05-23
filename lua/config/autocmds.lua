74a715118dcf7764e6922008ee1bb866b1f1668c
Stefan Maaßen <s.maassen@verband.creditreform.de>
2024-05-23 08:19:06 +0200

    switched to built-in inlay hints

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,30 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- enable LSP inlayhints for neovim < 0.10
-vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-  pattern = { "*.java", "*.rs", "*.js", "*.ts" },
-  group = "LspAttach_inlayhints",
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
-
-    if vim.fn.has("nvim-0.10") == 1 then
-      -- neovim 0.10 has inlay hints without external plugin
-      return
-    end
-
-    local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local status_lspinlay, lsp_inlayhints = pcall(require, "lsp-inlayhints")
-    if status_lspinlay then
-      lsp_inlayhints.on_attach(client, bufnr)
-    end
-  end,
-})
-
 -- refresh code lens automatically
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   pattern = { "*.java", "*.rs", "*.js", "*.ts" },
