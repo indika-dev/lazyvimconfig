619230accec6548fed065b2a6517a6fe4aad00e0
Stefan Maaßen <stefan.maassen@posteo.de>
2024-07-09 21:19:20 +0200

    jvm for jdtls is jdk17

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -183,10 +183,10 @@ return {
           return vim.fn.glob(jdtls_install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
         end,
         java_home = function()
-          return vim.env.HOME .. "/.local/lib/jvm-21"
+          return vim.env.HOME .. "/.local/lib/jvm-17"
         end,
         jdtls_jvm_home = function()
-          return vim.env.HOME .. "/.local/lib/jvm-21"
+          return vim.env.HOME .. "/.local/lib/jvm-17"
         end,
         -- Where are the config and workspace dirs for a project?
         jdtls_config_dir = function(project_name)
