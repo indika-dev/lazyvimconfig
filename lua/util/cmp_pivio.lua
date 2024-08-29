948b14091406665f93b5a883c77b01deb8b8a93d
Stefan Maaßen <s.maassen@verband.creditreform.de>
2024-08-29 11:30:27 +0200

    initial import for adding snippets of my own

---
+++ lua/util/cmp_pivio.lua
@@ -0,0 +1,32 @@
+local source = {}
+
+function source:is_available()
+  -- Only returns `true` if the buffer is a markup file inside my blog directory.
+  return vim.bo.filetype == "toml"
+end
+
+function source:complete(params, callback)
+  local items = {}
+
+  -- There's also the `cursor_after_line`, `cursor_line`, and a `cursor` fields on `context`.
+  local cursor_before_line = params.context.cursor_before_line
+
+  -- Only complete if there's a `/` anywhere before the cursor.
+  if cursor_before_line:sub(1, 1) == "/" then
+    items = {
+      { label = "/one" },
+      { label = "/two" },
+    }
+  end
+
+  -- `callback` should always be called.
+  callback(items)
+end
+
+-- Trigger completion (i.e. open up cmp) on these characters.
+-- We can also trigger it manually, see `:help cmp.mapping.complete`.
+function source:get_trigger_characters()
+  return { "/" }
+end
+
+return source
