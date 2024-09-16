2d038fbaf79ce9820be43105f3dd5fa45ebad60a
Stefan Maaßen <s.maassen@verband.creditreform.de>
2024-09-16 10:33:52 +0200

    added kitty-scrollback

--- lua/plugins/kitty.lua
+++ lua/plugins/kitty.lua
@@ -5,4 +5,17 @@ return {
   {
     "knubie/vim-kitty-navigator",
   },
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    opts = function()
+      search = {
+        callbacks = {
+          after_ready = function()
+            vim.api.nvim_feedkeys("?", "n", false)
+          end,
+        },
+      }
+      return search
+    end,
+  },
 }
