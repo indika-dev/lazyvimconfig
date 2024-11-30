1e2df4ada2c3537f37c21d6bf87d275c0e3f8bf8
Stefan Maaßen <stefan.maassen@posteo.de>
2024-12-01 00:39:13 +0100

    deeper integration with mason-registry

--- lua/plugins/springBoot.lua
+++ lua/plugins/springBoot.lua
@@ -1,3 +1,22 @@
+---@return string|nil path_to_ls path to directory of spring-boot language server or nil for using default value
+local try_jar_from_mason = function(package_name, key_prefix)
+  local success, mason_registry = pcall(require, "mason-registry")
+  if success then
+    local mason_package = mason_registry.get_package(package_name)
+    if mason_package:is_installed() then
+      local install_path = mason_package:get_install_path()
+      mason_package:get_receipt():if_present(function(recipe)
+        for key, value in pairs(recipe.links.share) do
+          if key:sub(1, #key_prefix) == key_prefix then
+            return vim.fn.fnamemodify(install_path .. "/" .. value, ":h")
+          end
+        end
+      end)
+    end
+  end
+  return nil
+end
+
 return {
   {
     "JavaHello/spring-boot.nvim",
@@ -6,11 +25,14 @@ return {
       "mfussenegger/nvim-jdtls",
     },
     opts = function()
-      require("spring_boot").init_lsp_commands()
       return {
-        -- ls_path = vim.g.spring_boot.jdt_extensions_path .. "/language-server",
+        ls_path = try_jar_from_mason("vscode-spring-boot-tools", "vscode-spring-boot-tools/language-server.jar"),
         java_cmd = vim.env.HOME .. "/.local/lib/semeru-17/bin/java",
       }
+    end,
+    setup = function(_, opts)
+      require("spring_boot").init_lsp_commands()
+      require("spring_boot").setup(opts)
     end,
   },
 }
