4037cb047c59464981682317306c341aba809559
Stefan Maaßen <stefan.maassen@posteo.de>
2024-12-09 22:53:02 +0100

    added jproperties for spring-boot as ft

--- lua/plugins/springBoot.lua
+++ lua/plugins/springBoot.lua
@@ -2,7 +2,7 @@ return {
   {
     -- "JavaHello/spring-boot.nvim",
     dir = "~/workspace/spring-boot.nvim",
-    ft = "java",
+    ft = { "java", "jproperties" },
     opts = function()
       return {
         java_cmd = vim.env.HOME .. "/.local/lib/semeru-17/bin/java",
