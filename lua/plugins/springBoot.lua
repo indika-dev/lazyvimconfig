2e7cc823c8ececfac10638851b710a47c4241c11
Stefan Maaßen <s.maassen@verband.creditreform.de>
2024-12-20 12:49:24 +0100

    fixed for better performance and better filetype handling

--- lua/plugins/springBoot.lua
+++ lua/plugins/springBoot.lua
@@ -2,8 +2,14 @@ return {
   {
     -- "JavaHello/spring-boot.nvim",
     dir = "~/workspace/spring-boot.nvim",
-    ft = "java",
+    ft = "jproperties",
     opts = function()
+      require("mason").setup()
+      local success, mason_registry = pcall(require, "mason-registry")
+      if success then
+        vim.notify("refreshing mason registry", vim.log.levels.INFO)
+        mason_registry.refresh()
+      end
       return {
         java_cmd = vim.env.HOME .. "/.local/lib/semeru-17/bin/java",
       }
