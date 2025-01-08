afc66a5e6f2fc84c1b278c770226d7e8e01c109a
Stefan Maaßen <s.maassen@verband.creditreform.de>
2025-01-08 17:35:01 +0100

    added plugins for Spring Initializr

---
+++ lua/plugins/springboot-start.lua
@@ -0,0 +1,20 @@
+if true then
+  return {}
+end
+return {
+  {
+    "niT-Tin/springboot-start.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("springboot-start").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
+}
