3490f5a9080d9278160d16d2e2a8be993244a2e2
Stefan Maaßen <s.maassen@verband.creditreform.de>
2025-01-08 17:34:41 +0100

    disabled lspconfig and reassigned local mason registry

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,28 +3,27 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(
-        opts.ensure_installed,
-        {
-          "codelldb",
-          "stylua",
-          "shfmt",
-          "jsonlint",
-          "fixjson",
-          "bash-language-server",
-          "lemminx",
-          "vscode-java-decompiler",
-          "yamlfmt",
-          "yamllint",
-          "djlint",
-          "jdtls",
-          "yq",
-        }
-      )
+      vim.list_extend(opts.ensure_installed, {
+        "codelldb",
+        "stylua",
+        "shfmt",
+        "jsonlint",
+        "fixjson",
+        "bash-language-server",
+        "lemminx",
+        "vscode-java-decompiler",
+        "yamlfmt",
+        "yamllint",
+        "djlint",
+        "jdtls",
+        "yq",
+      })
       opts.registries = {
         "file:~/workspace/mason-registry",
-        -- "github:indika-dev/mason-registry",
+        -- "github:mason-org/mason-registry",
       }
+      --   -- "github:nvim-java/mason-registry",
+      --   -- "github:mason-org/mason-registry",
     end,
   },
 }
