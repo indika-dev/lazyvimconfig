b9f91950d04ebca4e951591fb9bc9667cd05f74c
Stefan Maaßen <s.maassen@verband.creditreform.de>
2025-01-14 10:15:22 +0100

    removed annoying message and switched finally back to original plugin

--- lua/plugins/springBoot.lua
+++ lua/plugins/springBoot.lua
@@ -1,17 +1,16 @@
 return {
   {
-    -- "JavaHello/spring-boot.nvim",
-    dir = "~/workspace/spring-boot.nvim",
+    "JavaHello/spring-boot.nvim",
     ft = { "jproperties" },
     opts = function()
       require("mason").setup()
       local success, mason_registry = pcall(require, "mason-registry")
       if success then
-        vim.notify("refreshing mason registry", vim.log.levels.INFO)
         mason_registry.refresh()
       end
       return {
         java_cmd = vim.env.HOME .. "/.local/lib/semeru-17/bin/java",
+        autocmd = true,
       }
     end,
   },
