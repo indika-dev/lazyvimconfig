60815f9602401bb65d21e198480c2060cd1fffd4
Stefan Maaßen <s.maassen@verband.creditreform.de>
2025-01-16 08:49:21 +0100

    added user command

--- lua/plugins/springBoot.lua
+++ lua/plugins/springBoot.lua
@@ -8,9 +8,12 @@ return {
       if success then
         mason_registry.refresh()
       end
+      vim.api.nvim_create_user_command("SpringBootSymbols", function(_)
+        -- vim.cmd({ cmd = "FzfLua", args = { "lsp_live_workspace_symbols" } })
+        require("fzf-lua").lsp_live_workspace_symbols()
+      end, { nargs = 0 })
       return {
         java_cmd = vim.env.HOME .. "/.local/lib/semeru-17/bin/java",
-        autocmd = true,
       }
     end,
   },
