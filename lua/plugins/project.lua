53334ad870e4880c631de5732ba061bbc6e74c67
Stefan Maaßen <s.maassen@verband.creditreform.de>
2025-04-04 09:44:47 +0200

    sync root dir with project root dir

---
+++ lua/plugins/project.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = true,
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+    },
+  },
+}
