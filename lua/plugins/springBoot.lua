8f006531569661ce9c2251f8838ce1aefa5f9c23
Stefan Maassen <stefan.maassen@posteo.de>
2026-05-06 15:35:11 +0200

    reenabled springboot

--- lua/plugins/springBoot.lua
+++ lua/plugins/springBoot.lua
@@ -1,20 +1,20 @@
-return {}
---   {
---     "JavaHello/spring-boot.nvim",
---     dev = { true },
---     ft = { "jproperties", "java" },
---     opts = function()
---       require("mason").setup()
---       local success, mason_registry = pcall(require, "mason-registry")
---       if success then
---         mason_registry.refresh()
---       end
---       vim.api.nvim_create_user_command("SpringBootSymbols", function(_)
---         require("fzf-lua").lsp_live_workspace_symbols()
---       end, { nargs = 0 })
---       return {
---         java_cmd = vim.env.HOME .. "/.local/lib/jvm-jdtls/bin/java",
---       }
---     end,
---   },
--- }
+return {
+  {
+    "JavaHello/spring-boot.nvim",
+    dev = { true },
+    ft = { "jproperties", "java" },
+    opts = function()
+      require("mason").setup()
+      local success, mason_registry = pcall(require, "mason-registry")
+      if success then
+        mason_registry.refresh()
+      end
+      vim.api.nvim_create_user_command("SpringBootSymbols", function(_)
+        require("fzf-lua").lsp_live_workspace_symbols()
+      end, { nargs = 0 })
+      return {
+        java_cmd = vim.env.HOME .. "/.local/lib/jvm-jdtls/bin/java",
+      }
+    end,
+  },
+}
