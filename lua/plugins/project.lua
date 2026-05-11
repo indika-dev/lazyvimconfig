ec911ee9c4f3460d569ee0545443c9980e9be629
Stefan Maassen <stefan.maassen@posteo.de>
2026-05-11 08:39:47 +0200

    merged wsl2 and main

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,28 +1,28 @@
-return {
-  {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup({})
-    end,
-    -- opts = {
-    --   manual_mode = true,
-    --   sync_root_with_cwd = true,
-    --   respect_buf_cwd = true,
-    --   update_focused_file = {
-    --     enable = true,
-    --     update_root = true,
-    --   },
-    -- },
-  },
-  -- {
-  --   "nvim-tree",
-  --   opts = {
-  --     sync_root_with_cwd = true,
-  --     respect_buf_cwd = true,
-  --     update_focused_file = {
-  --       enable = true,
-  --       update_root = true,
-  --     },
-  --   },
-  -- },
-}
+return {}
+-- {
+-- "ahmedkhalf/project.nvim",
+-- config = function()
+--   require("project_nvim").setup({})
+-- end,
+-- opts = {
+--   manual_mode = true,
+--   sync_root_with_cwd = true,
+--   respect_buf_cwd = true,
+--   update_focused_file = {
+--     enable = true,
+--     update_root = true,
+--   },
+-- },
+-- },
+-- {
+--   "nvim-tree",
+--   opts = {
+--     sync_root_with_cwd = true,
+--     respect_buf_cwd = true,
+--     update_focused_file = {
+--       enable = true,
+--       update_root = true,
+--     },
+--   },
+-- },
+-- }
