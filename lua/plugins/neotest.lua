f775f74155307abc4055bb2277e78e069f25538c
Stefan Maassen <stefan.maassen@posteo.de>
2026-06-02 09:40:17 +0200

    setup neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,22 +1,13 @@
 return {
+  { "nvim-neotest/neotest-plenary" },
+  -- { "rcasia/neotest-java" },
+  { "atm1020/neotest-jdtls" },
+  { "beneeng/neotest-gradle" },
+  -- {   "weilbith/neotest-gradle"},
+  { "codymikol/neotest-kotlin" },
   {
     "nvim-neotest/neotest",
     event = "LspAttach",
-    dependencies = {
-      "rcasia/neotest-java",
-      "atm1020/neotest-jdtls",
-      "weilbith/neotest-gradle",
-      "codymikol/neotest-kotlin",
-      "nvim-neotest/neotest-plenary",
-    },
-    opts = function()
-      return {
-        adapters = {
-          ["neotest-jdtls"] = {},
-          ["neotest-kotlin"] = {},
-          ["neotest-plenary"] = {},
-        },
-      }
-    end,
+    opts = { adapters = { "neotest-jdtls", "neotest-kotlin", "neotest-plenary" } },
   },
 }
