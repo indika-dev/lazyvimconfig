7a4c73e40493d8229e2eec6789f5854f6d35e85d
Stefan Maassen <stefan.maassen@posteo.de>
2026-06-02 09:39:57 +0200

    enable gradle plugin

---
+++ lua/plugins/gradle.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "oclay1st/gradle.nvim",
+    cmd = { "Gradle", "GradleExec", "GradleInit", "GradleFavorites" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      gradle_executable = "./gradlew",
+    }, -- options, see default configuration
+    keys = {
+      { "<leader>G", desc = "+Gradle", mode = { "n", "v" } },
+      { "<leader>Gg", "<cmd>Gradle<cr>", desc = "Gradle Projects" },
+      { "<leader>Gf", "<cmd>GradleFavorites<cr>", desc = "Gradle Favorite Commands" },
+    },
+  },
+}
