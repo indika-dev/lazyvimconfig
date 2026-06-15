314e23d9fba1be2540b92ff6429180a3da9d7fe4
Stefan Maassen <stefan.maassen@posteo.de>
2026-06-15 11:20:00 +0200

    ignore exitcode

--- lua/extras/lang/kotlin.lua
+++ lua/extras/lang/kotlin.lua
@@ -142,7 +142,7 @@ return {
           stdin = false, -- or false if it doesn't support content input via stdin. In that case the filename is automatically added to the arguments.
           args = { "-bp " }, -- list of arguments. Can contain functions with zero arguments that will be evaluated once the linter is used.
           stream = "stdout", -- ('stdout' | 'stderr' | 'both') configure the stream to which the linter outputs the linting result.
-          ignore_exitcode = false, -- set this to true if the linter exits with a code != 0 and that's considered normal.
+          ignore_exitcode = true, -- set this to true if the linter exits with a code != 0 and that's considered normal.
           env = nil, -- custom environment table to use with the external process. Note that this replaces the *entire* environment, it is not additive.
           parser = require("lint.parser").from_pattern(
             "([^:]+):(%d+):(%d+): ([^[]+) %[(.+)%]",
